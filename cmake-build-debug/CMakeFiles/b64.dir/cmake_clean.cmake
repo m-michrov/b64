4c5bd89cf7a73e2efd85d93734b152285d186bb2
m-michrov <45308469+m-michrov@users.noreply.github.com>
2019-02-02 15:10:22 +0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/b64.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/b64.dir/main.c.obj"
+  "b64.pdb"
+  "b64.exe"
+  "b64.exe.manifest"
+  "libb64.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/b64.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
